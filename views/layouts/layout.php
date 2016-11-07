aefd06510d0ea4e8d150f3104ca3847854a8b10f
ampleeve <e@ampleev.com>
2016-11-07 19:22:10 +0300

    xzczxc

--- views/layouts/layout.php
+++ views/layouts/layout.php
@@ -17,47 +17,35 @@
         <table id="main" border="1px">
             <tr id="header">
                <td colspan="2">
-                   &nbsp;
+                   <img id="logo" src="../i/logo.png">
                </td>
             </tr>
             <tr id="menu">
                 <td colspan="2">
-                    &nbsp;
+                    <ul>
+                        <li><a href="/">Главная</a></li>
+                        <li><a href="/?controller=articles&action=Articles">Статьи</a></li>
+                        <li><a href="/?controller=catalog&action=Catalog">Каталог</a></li>
+                        <li><a href="/?controller=gallery&action=Gallery">Галерея изображений</a></li>
+                        <li><a href="/?controller=registration&action=Registration">Регистрация</a></li>
+                        <li><a href="/?controller=contacts&action=Contacts">Контакты</a></li>
+                    </ul>
                 </td>
             </tr>
             <tr id="content">
                 <td id="left">
-                    &nbsp;
+                    Left content
                 </td>
                 <td id="center">
-                    &nbsp;
+                    <?= $content; ?>
                 </td>
             </tr>
             <tr id="footer">
                 <td colspan="2">
-                    &nbsp;
+                    <p>© Gileson, 2015 - 2016</p>
                 </td>
             </tr>
         </table>
         </body>
 
 </html>
-        <!--<div>
-            <ul>
-                <li><a href="/">Главная</a></li>
-                <li><a href="/?controller=articles&action=Articles">Статьи</a></li>
-                <li><a href="/?controller=catalog&action=Catalog">Каталог</a></li>
-                <li><a href="/?controller=gallery&action=Gallery">Галерея изображений</a></li>
-                <li><a href="/?controller=registration&action=Registration">Регистрация</a></li>
-                <li><a href="/?controller=contacts&action=Contacts">Контакты</a></li>
-            </ul>
-
-        </div>
-        <div><?= $content; ?></div>
-
-        <footer >
-            <div>
-                <p>© Gileson, 2015 - 2016</p>
-            </div>
-        </footer>
-    -->
